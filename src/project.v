108f2c35d033045ce4b4a6c139f3c05f9f9ebbaf
A.S. <info@solvlsi.org>
2026-08-30 20:00:26 -0700

    Rename module from tt_um_topmodule to tt_um_example

--- src/project.v
+++ src/project.v
@@ -34,7 +34,7 @@
 
 `default_nettype none
 
-module tt_um_topmodule (
+module tt_um_example (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
